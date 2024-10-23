e8856cdf6b83df67af39a96a55cc7de8b35d26b2
MAbdullah-dev <mabdullahwebdev@gmail.com>
2024-10-23 14:57:56 -0400

    tutor-profile

--- resources/views/livewire/tutor-prfile.blade.php
+++ resources/views/livewire/tutor-prfile.blade.php
@@ -1,52 +1,168 @@
-<style>
-    .hero-img {
-        background-image: url('{{ asset('assets/images/tutor-profile-hero.jpg') }}');
-    }
-</style>
 <div class="tutor-profile">
-    <section class="tutor-profile-hero">
+    {{-- <section class="tutor-profile-hero">
         <div class="container">
             <div class="inner position-relative">
                 <div class="hero-img"></div>
-                <div class="hero-overlay position-absolute"></div>
+                <div class="hero-overlay position-absolute">
+                    <div class="tutor-data d-flex align-items-start">
+                        <div class="profile-pic">
+                            <img src="{{ asset('assets/images/tutor-profile-image.jpg') }}" alt="">
+                        </div>
+                        <div class="tutor-detail ms-2 mt-5">
+                            <h3>Niki Shafer</h3>
+                            <span>9 courses</span>
+                            <span>.</span>
+                            <span>48 students</span>
+                        </div>
+                        <div class="ratings d-flex ms-auto align-items-center mt-5">
+                            <div class="rating-stars">
+                                <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                            </div>
+                            <span class="rating-digits">4.80 </span>
+                            <span>(10.00)</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </section>
-    <section class="tutor-profile">
+    </section> --}}
+    <section class="banner">
         <div class="container">
-            <div class="inner">
-                <div class="tutor-data d-flex align-items-start">
-                    <div class="profile-pic">
-                        <img src="{{ asset('assets/images/tutor-profile-image.jpg') }}" alt="">
-                    </div>
-                    <div class="tutor-detail ms-2 mt-5">
-                        <h3>Niki Shafer</h3>
-                        <span>9 courses</span>
-                        <span>.</span>
-                        <span>48 students</span>
-                    </div>
-                    <div class="ratings d-flex ms-auto align-items-center mt-5">
-                        <div class="rating-stars">
-                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
-                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
-                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
-                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
-                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
+            <div class="banner-image"
+                style="background-image: url('{{ asset('assets/images/tutor-profile-hero.jpg') }}'); background-size: cover">
+                <div class="row align-items-center z-1 d-none d-lg-block">
+                    <div class="col-lg-3">
+                        <div class="profile">
+                            <img src="{{ asset('assets/images/tutor-profile-image.jpg') }}" alt="">
                         </div>
-                        <span class="rating-digits">4.80 </span>
-                        <span>(10.00)</span>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="profile-details">
+                            <div class="details-profile">
+                                <h3>Niki Shafer</h3>
+                                <span>9 courses</span>
+                                <span>.</span>
+                                <span>48 students</span>
+                            </div>
+                            <div class="rating-details d-flex">
+                                <span>
+                                    <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                    <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                    <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                    <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                    <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                </span>
+                                <p>4.80 <span>(10.00)</span></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <div class="tutor-profile-content">
+    <section class="tutor-profile-content">
         <div class="container">
             <div class="inner">
+                <div class="row">
+                    <div class="col-md-12 col-lg-9 ms-auto">
+                        <div class="profile-details-mobile d-block d-lg-none d-flex align-items-center justify-content-center flex-column">
+                            <div class="profile">
+                                <img src="{{ asset('assets/images/tutor-profile-image.jpg') }}" alt="">
+                            </div>
+                            <div class="profile-details d-flex align-items-center justify-content-center flex-column">
+                                <div class="details-profile">
+                                    <h3>Niki Shafer</h3>
+                                    <span>9 courses</span>
+                                    <span>.</span>
+                                    <span>48 students</span>
+                                </div>
+                                <div class="rating-details d-flex">
+                                    <span>
+                                        <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                        <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                        <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                        <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                        <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                    </span>
+                                    <p>4.80 <span>(10.00)</span></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tutor-user-profile-content tutor-d-block mt-0 mt-lg-5 px-3">
+                            <h3>Biography</h3>
+                            <p>Ambrose&nbsp; &nbsp;is a dedicated nurse and healthcare leader with extensive experience
+                                in psychiatric hospitals, correctional facilities, and nursing homes before joining the
+                                Freedom House Family. Starting his career in Boston, he later became a travel nurse in
+                                North Carolina, gaining valuable insights into mental health and substance misuse and
+                                rising to become the director of the facility-based crisis unit. Ambrose leads a team
+                                dedicated to providing high-quality care to individuals in crisis, aiming to empower
+                                them to reintegrate into their communities. Beyond his professional life, Ambrose is a
+                                devoted family man, driven by personal experiences to make a positive impact in mental
+                                health care, advocating for healing, recovery, and resilience.</p>
 
+                            <h3>Courses</h3>
+                            <div class="tutor-pagination-wrapper-replaceable">
+                                <div class="tutor-course-list tutor-grid tutor-grid-3">
+                                    <p>No course yet.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row gy-4">
+                            <!-- Repeat card structure for course cards -->
+                            <div class="col-md-6  col-xl-4">
+                                <div class="card shadow">
+                                    <div class="card-img-top">
+                                        <img src="{{ asset('assets/images/featured-card-1.png') }}" class="card-img-top"
+                                            alt="...">
+                                        <i class="fa-regular fa-bookmark"></i>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">Recovery-Oriented System of Care</h5>
+                                        <div class="tutor-ratings d-flex align-items-center gap-1">
+                                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                            <i class="fa-regular fa-star" style="color: #eea015;"></i>
+                                        </div>
+                                        <div class="tutor-meta-wrapper d-flex align-items-center gap-5">
+                                            <div class="tutor-data d-flex align-items-center gap-1">
+                                                <img src="{{ asset('assets/images/created_by.png') }}" alt="">
+                                                <small>by</small><span>Autumn Green</span>
+                                            </div>
+                                            <div class="tutor-meta-icon d-flex align-items-center gap-2">
+                                                <div class="meta-icon d-flex align-items-center">
+                                                    <i class="fa-regular fa-user" style="color: #757c8e;"></i>
+                                                    <span>11</span>
+                                                </div>
+                                                <div class="meta-icon d-flex align-items-center">
+                                                    <i class="fa-regular fa-clock" style="color: #757c8e;"></i>
+                                                    <span>2h</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <p class="card-text">A recovery-oriented system of care is a network of clinical
+                                            and nonclinical use recovery. While co…
+                                        </p>
+                                        <div class="post-details">
+                                            <center>
+                                                <span class="duration">2hr &nbsp;|&nbsp;</span>
+                                                <span class="date">Publish date: May 24 2024</span>
+                                            </center>
+                                        </div>
+                                        <a class="button-primary mt-3 d-block w-100 text-center" href="#">More
+                                            Details...</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-
-    </div>
+    </section>
 
 </div>
